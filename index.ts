3f6fe3d51033f1fdf8de4c6b6f55be13b57d0994
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-23 14:24:47 -0700

    Converted the packageInfo export to a default export

--- index.ts
+++ index.ts
@@ -1,35 +1,35 @@
-import { AgentTeam, TokenRingPackage } from "@tokenring-ai/agent";
-import { z } from "zod";
+import {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
+import {z} from "zod";
 import AIService from "./AIService.js";
 
 import * as chatCommands from "./chatCommands.ts";
 import ModelRegistry from "./ModelRegistry.js";
-import { ModelProviderConfigSchema, registerModels } from "./models.js";
-import packageJSON from "./package.json" with { type: "json" };
+import {ModelProviderConfigSchema, registerModels} from "./models.js";
+import packageJSON from "./package.json" with {type: "json"};
 
 export const AIClientConfigSchema = z.object({
-	defaultModel: z.string(),
-	models: z.record(z.string(), ModelProviderConfigSchema),
+  defaultModel: z.string(),
+  models: z.record(z.string(), ModelProviderConfigSchema),
 });
 
-export const packageInfo: TokenRingPackage = {
-	name: packageJSON.name,
-	version: packageJSON.version,
-	description: packageJSON.description,
-	async install(agentTeam: AgentTeam) {
-		agentTeam.addChatCommands(chatCommands);
-		agentTeam.services.register(new ModelRegistry());
+export default {
+  name: packageJSON.name,
+  version: packageJSON.version,
+  description: packageJSON.description,
+  async install(agentTeam: AgentTeam) {
+    agentTeam.addChatCommands(chatCommands);
+    agentTeam.services.register(new ModelRegistry());
 
-		const config = agentTeam.getConfigSlice("ai", AIClientConfigSchema);
-		if (!config) return;
-		await registerModels(
-			config.models,
-			agentTeam.services.requireItemByType(ModelRegistry),
-		);
+    const config = agentTeam.getConfigSlice("ai", AIClientConfigSchema);
+    if (!config) return;
+    await registerModels(
+      config.models,
+      agentTeam.services.requireItemByType(ModelRegistry),
+    );
 
-		agentTeam.addServices(new AIService({ model: config.defaultModel }));
-	},
-};
+    agentTeam.addServices(new AIService({model: config.defaultModel}));
+  },
+} as TokenRingPackage;
 
-export { createChatRequest } from "./chatRequestBuilder/createChatRequest.ts";
-export { default as ModelRegistry } from "./ModelRegistry.ts";
+export {createChatRequest} from "./chatRequestBuilder/createChatRequest.ts";
+export {default as ModelRegistry} from "./ModelRegistry.ts";
